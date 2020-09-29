63654561dd65cbc42d9b0e7a3bfea185eae18249
Stackbit <projects@stackbit.com>
2020-09-29 04:10:46 +0000

    initial commit by Stackbit

---
+++ src/components/FaqSection.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import _ from 'lodash';
+
+import {markdownify} from '../utils';
+
+export default class FaqSection extends React.Component {
+    render() {
+        let section = _.get(this.props, 'section', null);
+        return (
+            _.map(_.get(section, 'questions', null), (question, question_idx) => (
+            <section key={question_idx} className="content__row faq__section"  data-id={_.get(section, 'section_id', null)}>
+              <div className="faq__section-content-container">
+                <h2 className="faq__question">{_.get(question, 'question', null)}</h2>
+                <div className="faq__answer">
+                  {markdownify(_.get(question, 'answer', null))}
+                </div>
+              </div>
+            </section>
+            ))
+        );
+    }
+}
